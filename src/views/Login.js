719e5e72488887a2becf6098445d309d02499693
Dekrayzis <vincent.corleone@live.com>
2020-11-27 17:35:22 +0000

    Update to login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -23,11 +23,51 @@ const LoginScreen = () => {
 
   return (
     <div className="login">
-      <div className="login__container">
+      <div class="background">
+        <div class="ball-container orange">
+          <div class="ball orange" />/
+        </div>
+        <div class="ball-container red">
+          <div class="ball red" />
+        </div>
+        <div class="ball-container purple">
+          <div class="ball purple" />
+        </div>
+        <div class="ball-container green">
+          <div class="ball green" />
+        </div>
+      </div>
+      <div class="card">
+        <div class="form-container">
+          <div className="login__title">Sign in to CChat</div>
+          <div class="form-title">login</div>
+          <div class="input first">
+            <input id="email" />
+            <label class="title" for="email">
+              email
+            </label>
+          </div>
+          <div class="input">
+            <input id="password" />
+            <label class="title" for="password">
+              password
+            </label>
+          </div>
+          <div class="info">forgot your password</div>
+          <button className="btn-google-signIn" onClick={signIn}>
+            Sign in with Google
+          </button>
+        </div>
+        <div class="button-bar">
+          <button class="button register">register</button>
+          <button class="button sign-in">sign in</button>
+        </div>
+      </div>
+      <img className="app_logo" src={logo} alt="logo" />
+      {/* <div className="login__container">
         <img src={logo} alt="logo" />
         <div className="login__text">Sign in to CChat</div>
-        <button onClick={signIn}>Sign in with Google</button>
-      </div>
+      </div> */}
     </div>
   );
 };
